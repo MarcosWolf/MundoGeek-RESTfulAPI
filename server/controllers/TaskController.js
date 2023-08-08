ccb40007df8a81f79721b3a9dacc8beb51351c63
Marcos Vinícios <marcos_hardrock@live.com>
2023-08-07 22:58:32 -0300

    (Feat) Routes; (Feat) Controllers

---
+++ server/controllers/TaskController.js
@@ -0,0 +1,22 @@
+const database = require("../database/connection");
+const path = require("path");
+const basePath = path.join(__dirname, '../templates');
+
+class TaskController {
+    visualizarPosts(request, response) {
+        let query = "SELECT * FROM posts";
+        database.query(query, (err, result) => {
+            if (err) {
+                console.log(err);
+            } else {
+                //response.send(result);
+                console.log({result});
+                response.sendFile(`${basePath}/post.html`, {
+                    result
+                });
+            }
+        });
+    }
+}
+
+module.exports = new TaskController();
--- server/index.js
+++ server/index.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const cors = require("cors");
 
+require('dotenv').config();
+
 const router = require("./routes/routes");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 
-const path = require("path");
-
-require('dotenv').config();
+const path = require("path")
 
 app.use (
     express.urlencoded({
@@ -17,11 +17,11 @@ app.use (
     }),
 );
 
+app.use(cors());
 app.use(express.json());
+app.use(router);
 
-app.use(express.static('public'));
-
-const basePath = path.join(__dirname, 'routes');
+const basePath = path.join(__dirname, 'templates');
 
 app.get('/', (req, res) => {
     res.sendFile(`${basePath}/index.html`);
