08eb0318d75268bf4177e42df852dd01d91733a6
Marcos Vinícios <marcos_hardrock@live.com>
2023-08-12 20:31:41 -0300

    (Feat) Feed: Implementando Load More, ainda com bug

--- client/src/components/Content/Post.tsx
+++ client/src/components/Content/Post.tsx
@@ -39,26 +39,32 @@ const Post:React.FC = () => {
     const {getPost} = state;
 
     return (
-        <div className="post">
-            {
-                getPost.length > 0 && getPost.map(post => (
-                    <div className="post-container" key={post.postID}>
-                        <p className="post-category">{post.categoryNAME}</p>
-                        <h2 className="post-title">{post.postTITLE}</h2>
-                        <h3 className="post-subtitle">Lorem, ipsum dolor sit amet consectetur adipisicing elit.</h3>
-                        <div className="post-info">
-                            <img className="post-portrait" src="https://avatars.githubusercontent.com/u/26293082?s=400&u=0082394880a1718cd9145af005c010b727915257&v=4"/>
-                            <p className="post-author">Publicado por <strong>Marcos Vinícios</strong></p>
-                            <hr/>
-                            <p className="post-datetime">12 de agosto de 2023 às 15:34</p>
+        <>
+            <div className="post">
+                {
+                    getPost.length > 0 && getPost.map(post => (
+                        <div className="post-container" key={post.postID}>
+                            <p className="post-category">{post.categoryNAME}</p>
+                            <h2 className="post-title">{post.postTITLE}</h2>
+                            <h3 className="post-subtitle">Lorem, ipsum dolor sit amet consectetur adipisicing elit.</h3>
+                            <div className="post-info">
+                                <img className="post-portrait" src="https://avatars.githubusercontent.com/u/26293082?s=400&u=0082394880a1718cd9145af005c010b727915257&v=4"/>
+                                <p className="post-author">Publicado por <strong>Marcos Vinícios</strong></p>
+                                <hr/>
+                                <p className="post-datetime">12 de agosto de 2023 às 15:34</p>
+                            </div>
+                            <img className="post-image" src={basePath + post.postTHUMBNAIL}/>
+                            <p className="post-image-description">Image caption</p>
+                            <div className="post-content" dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(post.postCONTENT)}} />
                         </div>
-                        <img className="post-image" src={basePath + post.postTHUMBNAIL}/>
-                        <p className="post-image-description">Image caption</p>
-                        <div className="post-content" dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(post.postCONTENT)}} />
-                    </div>
-                ))
-            }
-        </div>
+                    ))
+                }
+            </div>
+
+            <div>
+                Veja mais
+            </div>
+        </>
     );
 }
 
--- client/src/components/Data/DataLastNews.tsx
+++ client/src/components/Data/DataLastNews.tsx
@@ -1,82 +1,28 @@
-import {useState, useEffect} from "react";
-import { Link } from "react-router-dom";
+import {useState} from "react";
 import Axios from 'axios';
 
-import { IPosts } from "../Models/IPosts";
+import PostList from "./PostList";
 
-const basePath = "/img/posts/";
+const initialPosts = [
 
-let pageX = 0;
+];
 
-interface IState {
-    loading: boolean,
-    page: number,
-    getPosts: IPosts[],
-    errorMsg: string
-}
+const DataLastNews: React.FC = () => {
+    const [currentPage, setCurrentPage] = useState(1);
+    const [hasMorePosts, setHasMorePosts] = useState(true);
 
-const DataLastNews:React.FC = () => {
-    const [state, setState] = useState<IState>({
-        loading: false,
-        page: 0,
-        getPosts: [] as IPosts[],
-        errorMsg: ''
-    })
+    const loadMorePosts = () => {
 
-    useEffect(() => {
-        const loadPosts = () => {
-            setState({...state, loading: true, page: pageX});
-
-            console.log("OI");
-
-            
-
-            console.log(state.page);
-
-            
-                Axios.get(`http://192.168.0.2:3000/lastnews/${state.page}&5`)
-                //Axios.get(`https://api-mundogeek.onrender.com/lastnews/`)
-                    .then((response) => setState({
-                        ...state, loading:false, getPosts:response.data
-                    }))
-                    .catch(err => setState({
-                        ...state, loading:false, errorMsg:err.message
-                    }));
-
-        }
-
-        loadPosts();
-    },[state.page, state.getPosts]);
-
-    const loadMore = () => {
-        pageX = pageX + 1;
-    }
-
-
-    const {getPosts, loading} = state;
+    };
 
     return (
         <>
-            { loading && <p>Carregando...</p>}
-            {
-                getPosts.length > 0 && getPosts.map( post => (
-                    <div className="feed-card" key={post.postID}>
-                        <div className="feed-img">
-                            <Link to={"post/" + post.postID}><img src={basePath + post.postTHUMBNAIL} /></Link>
-                        </div>
-                        <div className="feed-data">
-                            <Link to={"post/" + post.postID}><h2>{post.postTITLE}</h2></Link>
-                            <p>Data</p>
-                        </div>
-                    </div>
-                ))
-            }
-
-            <div className="feed-btn-container">
-                <button className="feed-btn" onClick={loadMore}>Ver mais</button>
-            </div>
+            <h1>Últimas Notícias</h1>
+            <PostList initialPosts={initialPosts} loadMorePosts={loadMorePosts} hasMorePosts={hasMorePosts} />
         </>
+
     );
-}
+
+};
 
 export default DataLastNews;
