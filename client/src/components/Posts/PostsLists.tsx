9621aaf62dc7b508414f52e96477340e26ea2725
Marcos Vinícios <marcos_hardrock@live.com>
2023-09-22 15:17:19 -0300

    (Feat) Inserido mensagem de aviso quando não encontra nenhum post

--- client/src/components/Posts/PostsLists.tsx
+++ client/src/components/Posts/PostsLists.tsx
@@ -1,33 +1,34 @@
 import { Link } from "react-router-dom";
 import { IPosts } from "../Models/IPosts";
-
 import { format } from "date-fns";
 
 const basePath = "/img/posts/";
 
 interface PostListProps {
-    posts: IPosts[];
+    posts: IPosts[] | null;
 }
 
-const PostList: React.FC<PostListProps> = ({ posts}) => {
+const PostList: React.FC<PostListProps> = ({ posts }) => {
+    
     return (
         <>
-            {posts.map(post => (
-                <div className="feed-card" key={post.postID}>
-                    <div className="feed-img">
-                        <Link to={`/post/${post.postID}`}>
-                            <img src={basePath + post.postTHUMBNAIL} />
-                        </Link>
+            {posts &&
+                posts.map(post => (
+                    <div className="feed-card" key={post.postID}>
+                        <div className="feed-img">
+                            <Link to={`/post/${post.postID}`}>
+                                <img src={basePath + post.postTHUMBNAIL} alt={post.postTITLE} />
+                            </Link>
+                        </div>
+                        <div className="feed-data">
+                            <Link to={`/post/${post.postID}`}>
+                                <h3>{post.categoryNAME}</h3>
+                                <h2>{post.postTITLE}</h2>
+                                <p>{format(new Date(post.postDATE), 'dd/MM/yyyy')}</p>
+                            </Link>
+                        </div>
                     </div>
-                    <div className="feed-data">
-                        <Link to={`/post/${post.postID}`}>
-                            <h3>{post.categoryNAME}</h3>
-                            <h2>{post.postTITLE}</h2>
-                            <p>{format(new Date(post.postDATE), 'dd/MM/yyyy')}</p>
-                        </Link>
-                    </div>
-                </div>
-            ))}
+                ))}
         </>
     );
 }
